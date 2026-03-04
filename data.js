a73d73f86d46d26317cddc3591215bf892f2f513
Emmalyn Chen <emmalynchen@emmalyns-air.lan>
2026-03-04 08:00:30 -0800

    daily price update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.FLIGHT_DATA = {
-  "lastUpdated": "2026-03-03T21:39:22.071313+00:00",
+  "lastUpdated": "2026-03-04T16:00:30.678410+00:00",
   "config": {
     "origin": "SFO",
     "destinations": [
@@ -27,6 +27,17 @@ window.FLIGHT_DATA = {
           "arrivalTime": "08-06-2026 06:50 PM",
           "flightNumbers": "LO 36, LO 335",
           "layover": "4 hr 20 min in Warsaw"
+        },
+        {
+          "timestamp": "2026-03-04T16:00:02.256890+00:00",
+          "price": 5779.0,
+          "carrier": "LOT",
+          "outStops": 1,
+          "outDuration": "18 hr 0 min",
+          "departureTime": "07-06-2026 03:50 PM",
+          "arrivalTime": "08-06-2026 06:50 PM",
+          "flightNumbers": "LO 36, LO 335",
+          "layover": "4 hr 20 min in Warsaw"
         }
       ]
     },
@@ -46,6 +57,17 @@ window.FLIGHT_DATA = {
           "arrivalTime": "09-06-2026 07:05 AM",
           "flightNumbers": "AC 756, AC 874",
           "layover": "1 hr 35 min in Montreal"
+        },
+        {
+          "timestamp": "2026-03-04T16:00:02.256890+00:00",
+          "price": 6256.0,
+          "carrier": "British Airways",
+          "outStops": 1,
+          "outDuration": "14 hr 25 min",
+          "departureTime": "08-06-2026 07:30 PM",
+          "arrivalTime": "09-06-2026 06:55 PM",
+          "flightNumbers": "BA 286, BA 310",
+          "layover": "2 hr 35 min in London"
         }
       ]
     },
@@ -65,6 +87,17 @@ window.FLIGHT_DATA = {
           "arrivalTime": "09-06-2026 06:50 PM",
           "flightNumbers": "LX 39, LX 644",
           "layover": "1 hr 40 min in Z\u00fcrich"
+        },
+        {
+          "timestamp": "2026-03-04T16:00:02.256890+00:00",
+          "price": 6106.0,
+          "carrier": "British Airways",
+          "outStops": 1,
+          "outDuration": "15 hr 5 min",
+          "departureTime": "08-06-2026 04:35 PM",
+          "arrivalTime": "09-06-2026 04:40 PM",
+          "flightNumbers": "BA 284, BA 308",
+          "layover": "3 hr 20 min in London"
         }
       ]
     },
@@ -77,6 +110,17 @@ window.FLIGHT_DATA = {
         {
           "timestamp": "2026-03-03T18:36:05.180719+00:00",
           "price": 4268.0,
+          "carrier": "LOT",
+          "outStops": 1,
+          "outDuration": "15 hr 20 min",
+          "departureTime": "10-06-2026 10:15 PM",
+          "arrivalTime": "11-06-2026 10:35 PM",
+          "flightNumbers": "LO 38, LO 231",
+          "layover": "2 hr 0 min in Warsaw"
+        },
+        {
+          "timestamp": "2026-03-04T16:00:02.256890+00:00",
+          "price": 4446.0,
           "carrier": "LOT",
           "outStops": 1,
           "outDuration": "15 hr 20 min",
@@ -103,6 +147,17 @@ window.FLIGHT_DATA = {
           "arrivalTime": "11-06-2026 10:35 PM",
           "flightNumbers": "LO 38, LO 231",
           "layover": "2 hr 0 min in Warsaw"
+        },
+        {
+          "timestamp": "2026-03-04T16:00:02.256890+00:00",
+          "price": 4282.0,
+          "carrier": "LOT",
+          "outStops": 1,
+          "outDuration": "15 hr 20 min",
+          "departureTime": "10-06-2026 10:15 PM",
+          "arrivalTime": "11-06-2026 10:35 PM",
+          "flightNumbers": "LO 38, LO 231",
+          "layover": "2 hr 0 min in Warsaw"
         }
       ]
     }
@@ -110,6 +165,6 @@ window.FLIGHT_DATA = {
   "alerts": [],
   "stats": {
     "lowestEver": 4268.0,
-    "checkCount": 1
+    "checkCount": 2
   }
 };
