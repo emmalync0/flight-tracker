71cdb322e7e41e6efdcfe9e453f0f9df2604c811
Emmalyn Chen <emmalynchen@emmalyns-air.lan>
2026-07-30 08:00:07 -0700

    daily price update

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 window.FLIGHT_DATA = {
-  "lastUpdated": "2026-07-29T15:00:06.050146+00:00",
+  "lastUpdated": "2026-07-30T15:00:06.759162+00:00",
   "config": {
     "origin": "SFO",
     "destinations": [
-      "GVA",
+      "BRU",
       "AMS",
-      "BRU"
+      "GVA"
     ],
     "cabin": "BUSINESS",
     "passengers": 2,
@@ -184,6 +184,6 @@ window.FLIGHT_DATA = {
   ],
   "stats": {
     "lowestEver": 4268.0,
-    "checkCount": 58
+    "checkCount": 59
   }
 };
